18a09b6333c02af94f8aa1ac04fd9cbac76c29eb
MatthewNice <matthewwalternice@gmail.com>
2023-10-18 12:33:52 -0500

    trying to simplify installation scripting

--- middleway_vsl/install.sh
+++ middleway_vsl/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo "=========================="
-echo "Installing App vsl"
+echo "Installing App middleway"
 
 # Here is where we perform installation of scripts, services, etc.
 echo " - Installing ROS packages for VSL..."
 
 LIBPANDA_SRC=$(cat /etc/libpanda.d/libpanda_src_dir)
 LIBPANDA_USER=$(cat /etc/libpanda.d/libpanda_usr)
+LAUNCH_FILE=middleway_vsl.launch
 
 source /home/$LIBPANDA_USER/.bashrc
 
@@ -22,5 +23,14 @@ popd
 
 runuser -l $LIBPANDA_USER -c /etc/libpanda.d/apps/vsl/installRosPackagesForMidVsl.sh
 
-echo "Installing VSL demo..."
-runuser -l $LIBPANDA_USER -c /etc/libpanda.d/apps/vsl/installMidVslController.sh
+echo "Installing Middleway VSL demo..."
+# runuser -l $LIBPANDA_USER -c /etc/libpanda.d/apps/vsl/installMidVslController.sh
+pushd /home/$LIBPANDA_USER/catkin_ws
+source devel/setup.sh
+rosrun robot_upstart install ${LAUNCH_FILE} --user root
+
+echo "Enabling can_to_ros startup script"
+sudo systemctl daemon-reload
+sudo systemctl enable middleway
+popd
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
